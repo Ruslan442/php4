1283facd9cfb139ab74f4fa1820a2ab144b6a36e
Ruslan442 <pruslan442@gmail.com>
2023-05-10 02:35:32 +0300

    sdasdadsadsa

--- auth.php
+++ auth.php
@@ -4,6 +4,25 @@
         header('Location: index.php');
         exit();
     }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Create database
+    $sql = "CREATE DATABASE php4";
+    if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+    } else {
+    echo "Error creating database: " . $conn->error;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- login.php
+++ login.php
@@ -1,23 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "password";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Create database
-$sql = "CREATE DATABASE php4";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
 
 $db = mysqli_connect('localhost','root','','php4');
 
